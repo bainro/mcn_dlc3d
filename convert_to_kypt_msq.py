22c3a20ff559ed5435deef63fbda7a2b5bab9f5a
Robert Bain <31903812+bainro@users.noreply.github.com>
2024-01-29 16:06:47 -0800

    Update convert_to_kypt_msq.py

--- convert_to_kypt_msq.py
+++ convert_to_kypt_msq.py
@@ -86,7 +86,7 @@ model, data, metadata, current_iter = kpms.load_checkpoint(
     project_dir, model_name, iteration=num_ar_iters)
 
 # modify kappa to maintain the desired syllable time-scale
-model = kpms.update_hypparams(model, kappa=1e5)
+model = kpms.update_hypparams(model, kappa=1e4)
 
 # run fitting for an additional 500 iters
 _model = kpms.fit_model(
